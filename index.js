475d8975a263377af24cd5f38bce763a4283eed9
killu4kun <mateeus.soarees@outlook.com>
2022-01-26 14:00:53 -0300

    cria conexao com db e inicia o model de products

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require('dotenv').config();
+const express = require('express');
+const { productsRoute } = require('./routes');
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);

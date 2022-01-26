edf993fcad7383423a3e9b0bc2d5aca9cf3afa1b
killu4kun <mateeus.soarees@outlook.com>
2022-01-26 14:03:50 -0300

    adiciona criação de produto

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,3 +1,14 @@
 const connection = require('./connection');
 
-const addProducts = 
+const addProducts = async ({ name, quantity }) => {
+    const [result] = await connection.execute(
+        'INSERT INTO StoreManager.products (name, quantity) VALUES (?, ?)',
+        [name, quantity],
+    );
+    return {
+        id: result.insertId,
+        name,
+        quantity,
+    };
+};
+

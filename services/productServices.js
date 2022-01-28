3da8d03301b3dc40d942acf31ba1b0b1e3d2a4c9
killu4kun <mateeus.soarees@outlook.com>
2022-01-28 14:15:32 -0300

    refatore product services para arrumar o lint

--- services/productServices.js
+++ services/productServices.js
@@ -1,31 +1,44 @@
 const Products = require('../models/productsModels');
 
-const nameLongMessage = '"name" length must be at least 5 characters long';
+const findProduct = async (name) => {
+    const product = await Products.FindProductByName(name);
+    if (product.length !== 0) return { code: 409, message: 'Product already exists' };
+    return {};
+};
 
-const isValid = async (name, quantity) => {
-  if (!name) return { code: 400, message: '"name" is required' };
-  if (name.length < 5) return { code: 422, message: nameLongMessage };
-  const resultFind = await Products.FindProductByName(name);
+const validQuantity = (quantity) => {
+    if (typeof quantity !== 'number' || quantity <= 0) return true;
+    return false;
+};
 
-  if (resultFind.length !== 0) { return { code: 409, message: 'Product already exists' }; }
-  if (quantity === undefined) { return { code: 400, message: '"quantity" is required' }; }
-  if (typeof quantity !== 'number' || quantity <= 0) {
-    return {
-      code: 422,
-      message: '"quantity" must be a positive number',
-    };
-  }
-  return {};
+const isValid = (name, quantity) => {
+    switch (true) {
+        case (name === undefined): return { code: 400, message: '"name" is required' };
+        case (name.length < 5): return {
+            code: 422,
+            message: '"name" length must be at least 5 characters long',
+        };
+        case (quantity === undefined): return { code: 400, message: '"quantity" is required' };
+        case (validQuantity(quantity)): return { 
+            code: 422, 
+            message: '"quantity" must be a number larger than or equal 1', 
+        };
+        default: return {};
+    }
 };
 
 const create = async (name, quantity) => {
-  const productIsValid = await isValid(name, quantity);
+    const productValid = isValid(name, quantity);
 
-  if (productIsValid.message) return productIsValid;
+    if (name) {
+        const product = await findProduct(name);
+        if (product.message) return product;
+    }
 
-  const productId = await Products.create(name, quantity);
+    if (productValid.message) return productValid;
 
-  return { code: 201, productId };
+    const product = await Products.AddProducts({ name, quantity });
+    return { code: 201, productId: product.id };
 };
 
 module.exports = {

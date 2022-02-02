2f69693ff677d21fe40f73df3550606f19c33ae6
killu4kun <mateeus.soarees@outlook.com>
2022-02-02 00:02:59 -0300

    cria products controller : get by id , create , getAll

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,13 +1,38 @@
-const Products = require('../services/productServices');
+const productService = require('../services/productService');
 
-const createProduct = async (req, res) => {
+const create = async (req, res) => {
   const { name, quantity } = req.body;
-  const { code, productId, message } = await Products.create(name, quantity);
 
-  if (message) return res.status(code).json({ message });
-  return res.status(code).json({ productId });
+  try {
+    const newProduct = await productService.create({
+      name,
+      quantity: Number(quantity),
+    });
+    res.status(201).json(newProduct);
+  } catch (err) {
+    if (err.message === 'Product already exists') {
+      return res.status(409).json({ message: err.message });
+    }
+    res.status(400).send(err);
+  }
+};
+
+const getById = async (req, res) => {
+  const { id } = req;
+
+  const foundProduct = await productService.getById(id);
+  if (!foundProduct) { return res.status(404).json({ message: 'Product not found' }); }
+
+  res.status(200).json(foundProduct);
+};
+
+const getAll = async (req, res) => {
+  const products = await productService.getAll();
+  return res.status(200).json(products);
 };
 
 module.exports = {
-  createProduct,
+  create,
+  getById,
+  getAll,
 };

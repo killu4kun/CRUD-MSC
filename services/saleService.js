4b27a25564b7c9734d71080767481611c909b2c0
killu4kun <mateeus.soarees@outlook.com>
2022-02-02 23:05:21 -0300

    cria e adicina todos os metodos de sales services

---
+++ services/saleService.js
@@ -0,0 +1,39 @@
+const saleModel = require('../models/saleModel');
+
+const serialize = (sale) => ({
+  productId: sale.product_id,
+  quantity: sale.quantity,
+});
+
+const addSales = async (sales) => {
+  const newSaleId = await saleModel.createNewSale();
+  const serializedSales = sales.map(serialize);
+
+  const promisesArray = serializedSales
+    .map((sale) => saleModel.addSaleProducts({ saleId: newSaleId, ...sale }));
+    // aguarda todas as sales serem adicionadas
+  await Promise.all(promisesArray); 
+  return newSaleId;
+};
+
+const getAll = async () => {
+  const sales = await saleModel.getAll();
+  return sales;
+};
+
+const getById = async (id) => {
+  const sale = await saleModel.getById(id);
+  return sale;
+};
+
+const update = async (saleInfo) => {
+  const updatedSale = await saleModel.update(saleInfo);
+  return updatedSale;
+};
+
+module.exports = {
+  addSales,
+  getAll,
+  getById,
+  update,
+};

5a6c81d6d0e95d87675082f6b7e62e26d6057c6f
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 10:28:27 -0300

    cria rotas de sales e products no index e seus devidos middlewares

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const productsController = require('./controllers/productsController');
 const salesController = require('./controllers/salesController');
 
+const ensureProductName = require('./middlewares/ensureProductName');
+const ensureProductId = require('./middlewares/ensureProductId');
+const ensureProductQuantity = require('./middlewares/ensureProductQuantity');
+const ensureSaleProdId = require('./middlewares/ensureSaleProdId');
+const ensureSaleQuantity = require('./middlewares/ensureSaleQuantity');
+
 const app = express();
 app.use(express.json());
 
@@ -11,7 +17,36 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.post('/products', productsController.createProduct);
+app.post(
+  '/products',
+  ensureProductQuantity,
+  ensureProductName,
+  productsController.createProduct,
+);
+
+app.get('/products', productsController.getAllProducts);
+
+app.get('/products/:id', ensureProductId, productsController.getProductById);
+
+app.put(
+  '/products/:id',
+  ensureProductId,
+  ensureProductQuantity,
+  ensureProductName,
+  productsController.updateProduct,
+);
+
+app.delete('products/:id', ensureProductId, productsController.deleteProduct);
+
+app.get('/sales', salesController.getAllSales);
+
+app.get('/sales/:id', salesController.getSaleById);
+
+app.use(ensureSaleProdId, ensureSaleQuantity);
+
+app.post('/sales', salesController.createSale);
+
+app.put('/sales/:id', salesController.updateSale);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);

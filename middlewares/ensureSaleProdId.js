ac3edf984b22649eb723c06255bfbaa93cf80a2f
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 00:35:36 -0300

    middlewares : salequantity

---
+++ middlewares/ensureSaleProdId.js
@@ -0,0 +1,12 @@
+const ensureSaleProductId = (req, res, next) => {
+    const sales = req.body;
+    if (!(sales instanceof Array)) {
+      return res.status(400).json({ message: '"product_id" is required' });
+    }
+    const someInvalid = sales.some((sale) => !('product_id' in sale));
+    if (someInvalid) return res.status(400).json({ message: '"product_id" is required' }); 
+  
+    next();
+  };
+  
+  module.exports = ensureSaleProductId;

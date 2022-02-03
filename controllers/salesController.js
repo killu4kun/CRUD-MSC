ef2400333ac8a64024ea0a821aef1aaa7b9f7165
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 01:09:31 -0300

    adiciona metodo add salesController

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,8 +1,16 @@
 const saleService = require('../services/saleService');
 const productService = require('../services/productServices');
 
-const addSalesService = (req,res) => {
+const addSalesController = (req, res) => {
     const sales = req.body;
 
-    const promises = sales.map(({product_id}) => productService.)
-}
+    const promises = sales.map(({ product_id }) => productService.productExists(product_id));
+    Promise.all(promises).then((productIds) => {
+        const ValidAll = productIds.every((isvalid) => isvalid);
+        if (!ValidAll) res.status(404).json({ message: 'Product not found' });
+    });
+};
+
+module.exports = {
+    addSalesController,
+};

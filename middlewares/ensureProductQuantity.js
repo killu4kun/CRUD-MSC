823ad5a27498813222ba9dfc4056f0b7f73be432
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 00:32:57 -0300

    middlewares : prod quantity

---
+++ middlewares/ensureProductQuantity.js
@@ -0,0 +1,15 @@
+const ensureProductQuantity = (req, res, next) => {
+    const { quantity } = req.body;
+    const quantityNum = Number(quantity);
+    if (quantity === undefined || quantity === null) {
+      return res.status(400).json({ message: '"quantity" is required' });
+    }
+    if (Number.isNaN(quantityNum) || quantityNum < 1) {
+      return res.status(422).json({
+        message: '"quantity" must be a number larger than or equal to 1' });
+    }
+  
+    next();
+  };
+  
+  module.exports = ensureProductQuantity;

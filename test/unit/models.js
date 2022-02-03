b341c1aa7f7e7c08104eb12167ec7d7c330e4416
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 13:28:30 -0300

    inicio do teste e commit para ver %

---
+++ test/unit/models.js
@@ -0,0 +1,6 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const connection = require('../../models/connection');
+const productModel = require('../../models/productModel');
+const saleModel = require('../../models/saleModel');

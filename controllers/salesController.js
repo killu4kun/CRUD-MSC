719a62636b791afbd10500d15064146e35d83477
killu4kun <mateeus.soarees@outlook.com>
2022-02-03 10:14:57 -0300

    cria metodo saleupdate e salesgetbyid no controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -18,7 +18,31 @@ const getSalesAll = async (req, res) => {
     return res.status(200).json(sales);
   };
 
+const getSalesById = async (req, res) => {
+    const { id } = req.params;
+
+    if (!id) return res.status(404).json({ message: 'Sale not found' });
+    
+    const sale = await saleService.saleGetById(id);
+    if (sale && sale.length) return res.status(200).json(sale);
+
+    return res.status(404).json({ message: 'Sale not found' });
+};
+
+const updateSales = async (req, res) => {
+    const { id } = req.params;
+    const [sale] = req.body;
+
+    const productIsExistent = await productService.productGetById(sale.product_id);
+    if (!productIsExistent) return res.status(404).json({ message: 'Sale not found' });
+    await saleService.saleUpdate({ id, ...sale });
+
+    return res.status(200).json({ saleId: id, itemUpdated: [{ id, ...sale }] });
+};
+
 module.exports = {
     addSalesController,
     getSalesAll,
+    getSalesById,
+    updateSales,
 };

dee123607ad9c6173232d0694becd5b30ce4c4fb
killu4kun <mateeus.soarees@outlook.com>
2022-02-02 23:36:30 -0300

    cria middlewares e validação validação de id de produto

---
+++ middlewares/ensureProductId.js
@@ -0,0 +1,12 @@
+const ensureProductId = (req, res, next) => {
+  const { id } = req.params || req.body;
+  const numId = Number(id);
+  
+  if (!numId) {
+    return res.status(404).json({ message: 'Product not found' });
+  }
+  req.id = numId;
+  next();
+};
+
+module.exports = ensureProductId;
